ff3756912808490e5fb02a1314fa9c8dcb5d8088
Dharma Raj Thanait <dharmarajthanait25@gmail.com>
2021-09-17 06:29:43 +0545

    Custom hook, Api fetch, form validation and onsubmit handeler completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route,Switch } from 'react-router-dom';
 import './App.css';
 import { Home } from './components/Home/Home';
+import { Resume } from './components/Resume/Resume';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/resume/:username' render={(props) => <Resume {...props}/>} />
       </Switch>
     </div>
   );
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './main.css'
+import { Main } from './Main'
+import useUserDetails from '../services/fetch-user'
+
+export const Resume = (props) => {
+    const { username } = props.match.params;
+    const { loading,userDetail,error} =useUserDetails(username);
+    console.log(userDetail);
+
+    const renderResume = () => {
+        if (loading) {
+          return <div className='loading'>Loading...</div>;
+        }
+    
+        if (error) {
+          return <div className="error">{error}</div>;
+        }
+    
+        return <Main userDetail={userDetail} />;
+      };
+
+
+    return (
+        <div>
+            Working {username}
+            {renderResume()}
+        </div>
+    )
+}
